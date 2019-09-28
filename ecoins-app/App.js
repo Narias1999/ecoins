3f5c47c3d355f6a15553ac3e4139b905a0c5b36b
Nicolás Arias <nicolasarias870@gmail.com>
2019-09-28 16:49:33 -0500

    Auto-generated commit

--- ecoins-app/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { WebView } from 'react-native-webview'
-
-export default function App() {
-  return (
-    <WebView source={{ uri: 'https://expo.io' }} />
-  );
-}
