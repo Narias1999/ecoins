2bb65ed8129536e04bfe12c17c245e265a37bf05
Nicolás Arias <nicolasarias870@gmail.com>
2019-09-28 13:31:33 -0500

    solving merge

--- ecoins/src/app/app.module.ts
+++ ecoins/src/app/app.module.ts
@@ -10,22 +10,16 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from '../environments/environment';
 import { LoginComponent } from './login/login.component';
 import { RegisterCiuComponent } from './register-ciu/register-ciu.component';
-<<<<<<< HEAD
 import { IniciarComponent } from './iniciar/iniciar.component';
-=======
 import { HomeComponent } from './home/home.component';
->>>>>>> 2106d25857f5fad2a3536c5203315bc25c37cc2c
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterCiuComponent,
-<<<<<<< HEAD
-    IniciarComponent
-=======
+    IniciarComponent,
     HomeComponent
->>>>>>> 2106d25857f5fad2a3536c5203315bc25c37cc2c
   ],
   imports: [
     NgbModule,
