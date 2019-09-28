96cf8d4dc0af50c42421cac3e5c35a328274345b
Nicolás Arias <nicolasarias870@gmail.com>
2019-09-28 14:02:55 -0500

    login first logic

--- ecoins/src/app/app-routing.module.ts
+++ ecoins/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component'
 import { RegisterCiuComponent } from './register-ciu/register-ciu.component'
 import { IniciarComponent } from './iniciar/iniciar.component'
+import { HomeComponent } from './home/home.component'
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterCiuComponent },
   { path: '', component: IniciarComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
--- ecoins/src/app/login/login.component.ts
+++ ecoins/src/app/login/login.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  conditions: boolean;
+  router: Router;
 
-  constructor() { }
+  constructor(router: Router) {
+    this.router = router
+    this.conditions = false
+  }
+  conditionsEvent($event) {
+    this.conditions = !this.conditions
+  }
+  doLogin() {
+    if (this.conditions) {
+      this.router.navigate(['/home']);
+    } else {
+      alert('Debes aceptar los terminos y condiciones para continuar')
+    }
+  }
 
   ngOnInit() {
   }
