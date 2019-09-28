bdc5095bda8f59309007e5e33640eb7cf6390d3f
Nicolás Arias <nicolasarias870@gmail.com>
2019-09-28 13:29:51 -0500

    routes

--- ecoins/src/app/app-routing.module.ts
+++ ecoins/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component'
 import { RegisterCiuComponent } from './register-ciu/register-ciu.component'
+import { IniciarComponent } from './iniciar/iniciar.component'
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterCiuComponent },
+  { path: '', component: IniciarComponent },
 ];
 
 @NgModule({
